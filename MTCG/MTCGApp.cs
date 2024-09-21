cba5062dfabe84519186a855593ed1e9613b6c24
Ortwinius <ortwin.baldauf@gmail.com>
2024-09-21 17:14:59 +0200

    Projektdateien hinzufügen.

---
+++ MTCG/MTCGApp.cs
@@ -0,0 +1,15 @@
+﻿/*
+Monster Trading Card Game - Main file
+*/
+using System;
+
+namespace MTCG
+{
+    class MTCGApp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
